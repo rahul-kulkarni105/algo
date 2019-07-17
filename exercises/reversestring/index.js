db596628fa13a2e1b14048b299f76de8406ac308
rahul-kulkarni105 <rahulkulkarni0510@gmail.com>
2019-07-16 22:18:10 -0500

    String reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reverseStr = '';
+  for (char of str) {
+    reverseStr = char + reverseStr;
+  }
+  return reverseStr;
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+
+// function reverse(str) {
+//   let testArray = [];
+//   testArray = str.split('');
+//   testArray.reverse();
+//   return testArray.join('');
+// }
