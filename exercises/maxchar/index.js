3e3d9f5cd2e0fe5d5ea7d23835c7321a8a052a04
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 15:25:38 -0500

    maxChar complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Building a character Map
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    // if (!charMap[char]) {
+    //   charMap[char] = 1;
+    // } else {
+    //   charMap[char]++;
+    // }
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  // for in loop for looping over objects.
+  // Objects start with o, so use IN which starts with i, NOT o.
+  // For strings use for of as above.
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
