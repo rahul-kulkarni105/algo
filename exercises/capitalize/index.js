0b83b415baf12fe468b22f6ba0c2dee73ad831c6
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 18:04:07 -0500

    capitalize complete

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// First solution
+// Slightly easier to read solution
+// function capitalize(str) {
+//   const words = [];
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ');
+// }
+
+
+// Second solution
+// a bit complex to read solution
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+// This may not resolve something like "?QUe tal?". Use first one in that case
 
 module.exports = capitalize;
