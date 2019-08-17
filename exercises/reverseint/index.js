1130cb6ae9b8bf96d80e885a06729f5dac172d39
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 14:58:23 -0500

    reverseInt complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//   const reversedString = n.toString().split('').reverse().join('');
+//   if (n < 0) {
+//     return parseInt(reversedString) * -1;
+//   } else {
+//     return parseInt(reversedString);
+//   }
+// }
+
+function reverseInt(n) {
+  const reversedString = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+  return parseInt(reversedString) * Math.sign(n);
+}
 
 module.exports = reverseInt;
