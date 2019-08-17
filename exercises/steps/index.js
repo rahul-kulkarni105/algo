fbaa3d1f8a17c60fc93133cde81e04c1ac4912bf
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 18:44:57 -0500

    steps complete - iterative and recursive solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,63 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// First solution
+// Iterative solution
+// function steps(n) {
+//   /**
+//    * from 0 to n iterate through rows
+//    * create an empty string
+//    * from 0 to n iterate thorugh the columns
+//    * if
+//    * the current column is equal to or less than the current row add # to stair
+//    * else
+//    * add a space to stair
+//    * console the output
+//    */
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     // This is inside the row for loop as we need consoles
+//     // as many as the number of rows
+//     console.log(stair);
+//   }
+// }
+
+
+// Second solution
+// Recursive solution
+/**
+ * Find out bare minimum piece of information needed to call function
+ * Find reasonable defaults to those minimum pieces of information
+ * Give defaults to optional parameters
+ * so if not passed you don't end up in infinite loop
+ * Check base case, is there any work left to do? if not return.
+ * If there is work left, call function again BUT
+ * make sure to arguments HAVE to change
+ */
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if (stair.length <= row) {
+  //   stair += '#';
+  // } else {
+  //   stair += ' ';
+  // }
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
