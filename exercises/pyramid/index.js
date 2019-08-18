a64bed32f38883a4aee8d41a7ec52c2525cf5df8
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 19:21:19 -0500

    pyramid complete - iterative and recursive solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,50 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Iterative solution
+// 2 * n - 1 -> this is the KEY! relation between row and column numbers increaing
+// function pyramid(n) {
+//   const midPoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let column = 0; column < 2 * n -1; column++) {
+//       // calculate the mid point/center index of array
+//       // what is middle index for columns?!
+//       if (midPoint - row <= column && midPoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+
+// Recursive solution
+function pyramid(n, row = 0, level = '') {
+  /**
+   * SET UP the base case correctly
+   */
+  if (row === n) {
+    return;
+  }
+
+  // column length is checked here with this formula
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row +1);
+  }
+
+  // Logic for adding # and ''
+  const midPoint  = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midPoint - row <= level.length && midPoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
