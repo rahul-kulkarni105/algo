8faadce72af87b09ff12d5270d06ab34fd3da12f
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-17 20:09:44 -0500

    vowels complete - includes and regex solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// First solution
+// function vowels(str) {
+//   let count = 0;
+//   const vowelArr = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowelArr.includes(char)) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
+
+// Second solution - more condensed version with RegEx
+function vowels(str) {
+  // Matches is either going to be an Array or null (if regex returns nothing)
+  const matches = str.match(/[aeiou]/gi);
+  // Null is a falsey value
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
