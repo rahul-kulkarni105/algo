d945dd92886f4f089584636f9695fadeecac6c44
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-18 19:43:04 -0500

    fib - recursive memoized function update

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,6 +9,9 @@
 //   fib(4) === 3
 
 // Iterative solution
+/**
+ * Linear runtime for this solution
+ */
 // function fib(n) {
 //   const result = [0, 1];
 
@@ -23,11 +26,55 @@
 //   return result[n];
 // }
 
-// Recursive solution
+// // Recursive solution
+// function fib(n) {
+//   /**
+//    * Time complexity of Fib recursive problem.
+//    * Any exponential time algo is a automatic strict NO.
+//    * Example fib of 50 with this will take extremely long time to complete.
+//    *
+//    * Memoization:
+//    * Store the arguments of each function call along with the result.
+//    * If the function is called again,
+//    * return the precomputed result, rather than running the function again.
+//    */
+
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// Recursive solution with Memoization - Generic version of Memoize
+
+function memoize(fn) {
+  const cache = {};
+  /**
+   *
+  */
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
 function fib(n) {
   /**
    * Time complexity of Fib recursive problem.
+   * Any exponential time algo is a automatic strict NO.
+   * Example fib of 50 with this will take extremely long time to complete.
    *
+   * Memoization:
+   * Store the arguments of each function call along with the result.
+   * If the function is called again,
+   * return the precomputed result, rather than running the function again.
    */
 
   if (n < 2) {
@@ -36,5 +83,11 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+/**
+ * Pass the function you need to memoize to the memoization function as a parameter
+ * Then assign it to same memoized version of the function, incasse this is a recursive solution.
+*/
+fib = memoize(fib);
 
 module.exports = fib;
