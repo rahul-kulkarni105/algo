dfc4cd2760dac3e5e206a0f3ace937fd00479a76
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2019-08-18 20:51:26 -0500

    linked list update 1

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,6 +2,32 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
+/**
+ * - Orderd collection of data
+ * - Collection containes no of different nodes
+ * - each node containes some data
+ * - and reference to next node.
+ *
+ * - Chain of nodes.
+ * - List of nodes has an order which is always maintained.
+ * - Unless it is changed explicitly.
+ *
+ * - Head node - first node
+ * - tail node - last node - no reference to any other node.
+ *
+ * - First part
+ * - "Data" tucked inside each node - can be string, number, array, object
+ *
+ * - Other part
+ * - "next" node reference.
+ *
+ * const nodeOne = { data: 123 };
+ *
+ * const nodeTwo = { data: 456 };
+ *
+ * nodeOne.next = nodeTwo; == Linked list.
+ */
+
 class Node {}
 
 class LinkedList {}
